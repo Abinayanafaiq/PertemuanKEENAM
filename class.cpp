7cb8bccc2a621f0a4eb3090cb39242124eedf6a0
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-04-28 10:16:23 +0700

    Add main function to class.cpp

--- class.cpp
+++ class.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+
+class Mobil {
+    string nama_mobil;
+    string tipe_mobil;
+    private:
+        int harga_mobil;
+    public:
+
+
+}
+
+int main(){
+
+}
