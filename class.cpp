6ec8694299df55753bbe34e63f998dc0c8c18198
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-04-28 10:19:28 +0700

    Implement input_data method in Mobil class to gather car details

--- class.cpp
+++ class.cpp
@@ -8,6 +8,14 @@ class Mobil {
     private:
         int harga_mobil;
     public:
+        void input_data(){
+            cout << "Masukkan nama mobil: ";
+            cin >> nama_mobil;
+            cout << "Masukkan tipe mobil: ";
+            cin >> tipe_mobil;
+            cout << "Masukkan harga mobil: ";
+            cin >> harga_mobil;
+        }
 
 
 }
